8b1acc7eb6c855841684dad1b0aec5f64fae3a55
MD Nahid Hasan Bhuiyan <nahid241-35-474@diu.edu.bd>
2025-12-29 09:27:12 +0600

    Create q.sh

---
+++ q.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+check_prime() {
+    n=$1
+    if (( n < 2 )); then
+        echo "Not Prime"
+        return
+    fi
+    for (( i=2; i*i<=n; i++ )); do
+        if (( n % i == 0 )); then
+            echo "Not Prime"
+            return
+        fi
+    done
+    echo "Prime"
+}
+
+check_leap() {
+    y=$1
+    if (( (y%400==0) || (y%4==0 && y%100!=0) )); then
+        echo "Yes, Leap Year"
+    else
+        echo "No, Not Leap Year"
+    fi
+}
+
+sum_series() {
+    n=$1
+    sum=0
+    for (( i=1; i<=n; i++ )); do
+        sum=$((sum+i))
+    done
+    echo "$sum"
+}
+
+read -p "Enter a number: " num
+
+while true
+do
+    echo "1. Check Prime"
+    echo "2. Check Leap Year"
+    echo "3. Print Sum of Series (1 to n)"
+    echo "4. Exit"
+    read -p "Choose one operation from above: " choice
+
+    case $choice in
+    1) check_prime $num ;;
+    2) check_leap $num ;;
+    3) sum_series $num ;;
+    4) exit ;;
+    *) echo "Invalid choice" ;;
+    esac
+done
